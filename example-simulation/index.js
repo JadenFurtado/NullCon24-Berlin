885d0fb0b5dd8239203b259953ef2011ccdb933d
jaden <furtadojaden@gmail.com>
2024-02-22 13:46:37 +0530

    Add basic simulation

--- example-simulation/index.js
+++ example-simulation/index.js
@@ -74,17 +74,32 @@ loadFile('shaders/utils.glsl').then(utils => {
 
     scene.add(mesh)
 
-    // const geometry = new THREE.PlaneGeometry( 200, 200 );
-    // geometry.rotateY(3.14)
-    // const material = new THREE.MeshBasicMaterial( {color: 0xffff00, side: THREE.DoubleSide} );
-    // const plane = new THREE.Mesh( geometry, material );
-    // scene.add( plane );
+    const geometry3 = new THREE.CircleGeometry(4.2, 200)
+    const geometry2 = new THREE.CircleGeometry(4.2, 200)
+    const material = new THREE.MeshBasicMaterial({
+      color: 0x66ccff,
+      side: THREE.DoubleSide
+    })
+    const tank3 = new THREE.Mesh(geometry3, material)
+    tank3.rotateX(Math.PI / 2)
+    tank3.geometry.translate(5,-14,0)
 
+    const tank2 = new THREE.Mesh(geometry2, material)
+    tank2.rotateX(Math.PI / 2)
+    tank2.geometry.translate(-6.7,-13,0)
+
+    scene.add(tank3)
+    scene.add(tank2)
+    let count = 0;
     function animate () {
       renderer.clear()
       renderer.render(scene, camera)
+      if(count<=800){
+      count+=1
+      tank3.geometry.translate(0,0,-0.02)
+      tank2.geometry.translate(0,0,-0.02)
+    }
       controls.update()
-
       window.requestAnimationFrame(animate)
     }
     animate()
